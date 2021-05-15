3bdf93f9887c48a8dcd59d8b5e978ed16fc90695
Alexander Zhidkov <azhidkov@gmail.com>
2021-05-15 16:30:24 +0800

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAX7z8C60wsmx071CEXI36afIdW0dRwecw",
+    authDomain: "team-bldr.firebaseapp.com",
+    databaseURL: "https://team-bldr.firebaseio.com",
+    projectId: "team-bldr",
+    storageBucket: "team-bldr.appspot.com",
+    messagingSenderId: "814914099009",
+    appId: "1:814914099009:web:204eba22ab3db03a61f3c0"
+  }
 };
