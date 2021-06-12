395b82c309359830d5aadea44e5f5677f5c3fc4e
Alexander Zhidkov <azhidkov@gmail.com>
2021-06-12 17:09:52 +0800

    admin home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Team } from '../models/team';
 import { TeamEvent } from '../models/team-event';
 import { TeamBuilderUser } from '../models/teamBuilderUser';
@@ -15,8 +14,9 @@ import { TeamBuilderUser } from '../models/teamBuilderUser';
 export class HomeComponent implements OnInit {
   user: TeamBuilderUser;
   teamDoc: AngularFirestoreDocument<Team>;
-  team: Observable<Team | undefined>;
+  team: Team;
   teamEvents: TeamEvent[];
+  isAdmin: boolean = false;
   isLoading = true;
 
   constructor(
@@ -35,8 +35,15 @@ export class HomeComponent implements OnInit {
       this.afs.doc<TeamBuilderUser>(`users/${user.uid}`).get().subscribe(u => {
         this.user = <TeamBuilderUser>u.data();
         this.teamDoc = this.afs.doc<Team>(`teams/${this.user.teamId}`);
-        this.team = this.teamDoc.valueChanges();
-        this.team.subscribe(() => this.isLoading = false);
+        this.teamDoc.valueChanges().subscribe((team) => {
+          this.isLoading = false;
+          if (!team) {
+            console.error('Team is falsy');
+            return;
+          }
+          this.team = team;
+          this.isAdmin = team.adminIds.includes(this.user.uid);
+        });
 
         this.afs.collection<TeamEvent>(`/teams/${this.user.teamId}/events`, ref => ref.orderBy('dateTime', 'desc'))
           .valueChanges({ idField: 'id' })
